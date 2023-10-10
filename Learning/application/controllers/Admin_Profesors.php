c95560026c547726d9c3382cfb96fef9ada004ed
alberto-vgs <alberto_b03@outlook.com>
2023-10-10 06:13:46 +0100

    admin profesor corrections

--- Learning/application/controllers/Admin_Profesors.php
+++ Learning/application/controllers/Admin_Profesors.php
@@ -61,187 +61,152 @@ class Admin_Profesors extends CI_Controller
 
 	function proces_profesors_form()
 	{
-		if ($this->input->is_ajax_request()) {
-			if ($this->input->post("option") && $this->input->post("code")) {
-				$extValidation = False;
-			} else {
-				$extValidation = True;
-			}
-			$this->form_validation->set_rules("inpName", "Name", "required|min_length[3]|max_length[60]");
-			$this->form_validation->set_rules("inpLastname", "Last Name", "required|min_length[5]|max_length[60]");
-			$this->form_validation->set_rules("inpBirthday", "Birthday", "required|date");
-			$this->form_validation->set_rules("inpGender", "Gender", "required");
-			$this->form_validation->set_rules("inpID", "ID", "required|min_length[8]|max_length[12]");
-			if ($this->form_validation->run()) {
-				if ($extValidation) {
-					$exist = $this->DAO->queryEntity("tb_users", $filter = array("IDUser" => $this->input->post('inpID')), TRUE);
-					if (!$exist) {
-						//echo json_encode($exist);
-						$data  = array(
-							"name_person" => $this->input->post('inpName'),
-							"lastname_person" => $this->input->post('inpLastname'),
-							"birthday_person" => $this->input->post('inpBirthday'),
-							"gender_person" => $this->input->post('inpGender'),
-							"creation_date_person" => "default",
-							"update_date_person" => "default",
+		if (!$this->input->is_ajax_request()) {
+			redirect('home');
+		}
+		$this->input->post("option") && $this->input->post("code") ? $extValidation = False : $extValidation = True;
+
+		$this->form_validation->set_rules("inpName", "Name", "required|min_length[3]|max_length[60]");
+		$this->form_validation->set_rules("inpLastname", "Last Name", "required|min_length[5]|max_length[60]");
+		$this->form_validation->set_rules("inpBirthday", "Birthday", "required|date");
+		$this->form_validation->set_rules("inpGender", "Gender", "required");
+		$this->form_validation->set_rules("inpID", "ID", "required|min_length[8]|max_length[12]");
+		if ($this->form_validation->run()) {
+			if ($extValidation) {
+				$exist = $this->DAO->queryEntity("tb_users", $filter = array("IDUser" => $this->input->post('inpID')), TRUE);
+				if (!$exist) {
+					$data  = array(
+						"name_person" => $this->input->post('inpName'),
+						"lastname_person" => $this->input->post('inpLastname'),
+						"birthday_person" => $this->input->post('inpBirthday'),
+						"gender_person" => $this->input->post('inpGender'),
+						"creation_date_person" => "default",
+						"update_date_person" => "default",
+					);
+					$email = $this->email();
+					$this->DAO->trans_begin();
+					$this->DAO->saveAndEditDats("tb_people", $data, NULL);
+					$password = $this->generatePassword(6);
+					$user_img = base_url() . "/resources/dist/img/" . $this->input->post('inpGender') == "M" ? "user_boy_one.webp" : "user_girl_one.webp";
+					$data     = array(
+						"id_user" => $this->DAO->obtain_id(),
+						"IDUser" => $this->input->post('inpID'),
+						"email_user" => $email,
+						"img_user" => $user_img,
+						"password_tem_user" => $password,
+						"password_user" => $password,
+						"type_user" => "Teacher",
+					);
+					$this->DAO->saveAndEditDats("tb_users", $data, NULL);
+					if ($this->DAO->trans_end()) {
+						$response = array(
+							"status" => "success",
+							"message" => "Register successful, password: " . $password
 						);
-						$email = $this->email();
-						$this->DAO->trans_begin();
-						$test    = $this->DAO->saveAndEditDats("tb_people", $data, NULL);
-						$user_id = $this->DAO->obtain_id();
-						//$email = $this->input->post('inpName')."-".$this->input->post('inpLastname').$user_id."@learning.edu";
-						$password = $this->generatePassword(6);
-						if ($this->input->post('inpGender') == "M") {
-							$user_img = base_url() . "/resources/dist/img/user_boy_one.webp";
-						} else {
-							$user_img = base_url() . "/resources/dist/img/user_girl_one.webp";
-						}
-						$data     = array(
-							"id_user" => $user_id,
-							"IDUser" => $this->input->post('inpID'),
-							"email_user" => $email,
-							"img_user" => $user_img,
-							"password_tem_user" => $password,
-							"password_user" => $password,
-							"type_user" => "Teacher",
-						);
-						$test     = $this->DAO->saveAndEditDats("tb_users", $data, NULL);
-						$complete = $this->DAO->trans_end();
-						if ($complete) {
-							$response = array(
-								"status" => "success",
-								"message" => "Register successful, password: " . $password
-							);
-						} else {
-							$response = array(
-								"status" => "error",
-								"errors" => $test,
-							);
-						}
 					} else {
 						$response = array(
 							"status" => "error",
-							"message" => "User already exist.",
+							"errors" => "Contact to the admin.",
 						);
 					}
 				} else {
-					if ($this->input->post("option") == "edit") {
-						$data     = array(
+					$response = array(
+						"status" => "error",
+						"message" => "User already exist.",
+					);
+				}
+			} else {
+				switch ($this->input->post("option")) {
+					case 'edit':
+						$data = array(
 							"name_person" => $this->input->post('inpName'),
 							"lastname_person" => $this->input->post('inpLastname'),
 							"birthday_person" => $this->input->post('inpBirthday'),
 							"gender_person" => $this->input->post('inpGender'),
 							"update_date_person" => "default",
 						);
-						$filter   = array(
+						$filter = array(
 							"id_person" => $this->input->post('code'),
 						);
 						$complete = $this->DAO->saveAndEditDats("tb_people", $data, $filter);
-						if ($complete) {
-							$response = array(
-								"status" => "success",
-								"message" => "Register successful",
-							);
-						} else {
-							$response = array(
-								"status" => "error",
-								"errors" => $this->form_validation->error_array(),
-							);
-						}
-					} else if ($this->input->post("option") == "inactivate") {
-						$data     = array(
+						break;
+					case 'inactivate':
+						$data = array(
 							"status_user" => "Inactive",
 						);
-						$filter   = array(
+						$filter = array(
 							"id_user" => $this->input->post('code'),
 						);
 						$complete = $this->DAO->saveAndEditDats("tb_users", $data, $filter);
-						if ($complete) {
-							$response = array(
-								"status" => "success",
-								"message" => "Action completed successfuly",
-							);
-						} else {
-							$response = array(
-								"status" => "error",
-								"message" => "There is a problem.",
-							);
-						}
-					} else if ($this->input->post("option") == "reactive") {
-						$data     = array(
+						break;
+					case 'reactive':
+						$data = array(
 							"status_user" => "Active",
 						);
-						$filter   = array(
+						$filter = array(
 							"id_user" => $this->input->post('code'),
 						);
 						$complete = $this->DAO->saveAndEditDats("tb_users", $data, $filter);
-						if ($complete) {
-							$response = array(
-								"status" => "success",
-								"message" => "Action completed successfuly",
-							);
-						} else {
-							$response = array(
-								"status" => "error",
-								"message" => "There is a problem.",
-							);
-						}
-					}
+						break;
 				}
-			} else {
-				$response = array(
-					"status" => "error",
-					"errors" => $this->form_validation->error_array(),
-				);
-			}
-			echo JSON_encode($response);
-		} else {
-			redirect('home');
-		}
-	}
-
-	function optionsProccess()
-	{
-		if ($this->input->is_ajax_request()) {
-			if ($this->input->post("option") == "inactivate") {
-				$extValidation = FALSE;
-			} else {
-				$extValidation = TRUE;
-			}
-			$this->form_validation->set_rules("code", "Code", "required");
-
-			if ($this->form_validation->run()) {
-
-				$data     = array(
-					"status_user" => $extValidation ? 'Active' : 'Inactive',
-					"update_date_user" => "default",
-				);
-				$filter   = array(
-					"id_user" => $this->input->post("code"),
-				);
-				$test     = $this->DAO->saveAndEditDats("tb_users", $data, $filter);
-				$complete = $this->DAO->trans_end();
 				if ($complete) {
 					$response = array(
 						"status" => "success",
-						"message" => "Action complete successful",
+						"message" => "Action completed successfuly",
 					);
 				} else {
 					$response = array(
 						"status" => "error",
-						"errors" => $test,
+						"message" => "There is a problem.",
 					);
 				}
+			}
+		} else {
+			$response = array(
+				"status" => "error",
+				"errors" => $this->form_validation->error_array(),
+			);
+		}
+		echo JSON_encode($response);
+	}
+
+	function optionsProccess()
+	{
+		if (!$this->input->is_ajax_request()) {
+			return ('home');
+		}
+		$this->input->post("option") == "inactivate" ? $extValidation = FALSE : $extValidation = TRUE;
+		$this->form_validation->set_rules("code", "Code", "required");
+
+		if ($this->form_validation->run()) {
+
+			$data   = array(
+				"status_user" => $extValidation ? 'Active' : 'Inactive',
+				"update_date_user" => "default",
+			);
+			$filter = array(
+				"id_user" => $this->input->post("code"),
+			);
+			$this->DAO->saveAndEditDats("tb_users", $data, $filter);
+			$complete = $this->DAO->trans_end();
+			if ($complete) {
+				$response = array(
+					"status" => "success",
+					"message" => "Action complete successful",
+				);
 			} else {
 				$response = array(
 					"status" => "error",
-					"errors" => $this->form_validation->error_array(),
+					"errors" => $test,
 				);
 			}
-			echo JSON_encode($response);
 		} else {
-			return ('home');
+			$response = array(
+				"status" => "error",
+				"errors" => $this->form_validation->error_array(),
+			);
 		}
+		echo JSON_encode($response);
 	}
 
 	function email()
